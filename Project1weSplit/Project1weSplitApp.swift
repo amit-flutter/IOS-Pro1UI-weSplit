0596c6715593f2a853bdd4fd8347c4f374970791
karmaln <amitprajapati.kamaln@gmail.com>
2021-10-12 17:23:57 +0530

    Initial Commit

---
+++ Project1weSplit/Project1weSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project1weSplitApp.swift
+//  Project1weSplit
+//
+//  Created by karmaln technology on 12/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Project1weSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
